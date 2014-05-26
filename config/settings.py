3aaa847318fbb79eebd61ca16036ffb7207aaa74
Willem <willemarf@gmail.com>
2014-05-25 23:58:34 -0300

    settings

--- config/settings.py
+++ config/settings.py
@@ -62,11 +62,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'copa',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '8889',
+        'NAME': 'album',
+        'USER': 'album',
+        'PASSWORD': 'CtfhsbdHKtKC',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
