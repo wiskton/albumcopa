29037b4d2e27f527352aca985c3c8cca5cfcd015
Willem <will@poteinterativo.com.br>
2014-05-28 10:17:07 -0300

    bootstrap

--- figurinhas/views.py
+++ figurinhas/views.py
@@ -4,8 +4,18 @@ from django.template import RequestContext
 from figurinhas.models import Figurinha
 
 def home(request):
+    home = True
+    return render_to_response('home.html', locals(), context_instance=RequestContext(request))
+
+def album(request):
 
     figurinhas = Figurinha.objects.all()
     
-    return render_to_response('home.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('album.html', locals(), context_instance=RequestContext(request))
+
+def repetidas(request):
+
+    figurinhas = Figurinha.objects.filter(quantidade__gt=0)
+    
+    return render_to_response('album.html', locals(), context_instance=RequestContext(request))
         
